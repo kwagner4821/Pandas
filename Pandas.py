70e5c03a5a9a0d40759816c72aea49749767bd67
kwagner4821 <kowagner@andrew.cmu.edu>
2020-10-26 21:19:30 -0400

    Plotting

--- Pandas.py
+++ Pandas.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+city = pd.read_csv("https://raw.githubusercontent.com/kwagner4821/Pandas/main/bea-2006(1).csv")
+
+print(city)
+y = city["pcgmp"]
+x = np.log(city["pop"])
+
+plt.scatter(x, y)
+plt.show()
